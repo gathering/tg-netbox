6a8dd0442ce37c94a8461b0c2c078c2c379c9dca
Sjur Fredriksen <sjurtf@ifi.uio.no>
2025-03-26 00:05:00 +0100

    add defaults

--- config_context.tf
+++ config_context.tf
@@ -1,6 +1,41 @@
-resource "netbox_config_context" "global_config" {
-  name = "global-config"
+resource "netbox_config_context" "global_context" {
+  name        = "global-context"
+  description = "Default config context for TG25"
+  sites       = [netbox_site.the_ship.id]
   data = jsonencode(
-    { "key" = "value" }
+    {
+      SNMP = {
+        community = "SkulleBruktTelemetri"
+        contact   = "Svettefesten i glassburet"
+        location  = "LEGGETIIID!"
+      },
+      domainName = "tg25.tg.no",
+      hashes = {
+        handle_api  = "FISK"
+        handle_root = "asdasdasd"
+        handle_tech = "snurrekanel"
+        tacacs      = "kanelsnurrr"
+      },
+      nameServers = [
+        "1.1.1.1",
+        "8.8.8.8",
+        "2001:4860:4860::8888",
+        "2001:4860:4860::8844"
+      ]
+      ntpServers = [
+        "129.240.2.6",
+        "129.240.2.42",
+        "2001:700:100:425::42",
+        "2001:700:100:2::6"
+      ]
+      oxidizedServers = [
+        "12.34.56.78",
+        "20aa::1"
+      ]
+      tacacsServers = [
+        "21.43.65.87",
+        "2000:9999::1"
+      ]
+    }
   )
 }
