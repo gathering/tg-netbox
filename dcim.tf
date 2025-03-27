d60663bb300ec9f1fef1cad743ed1a6592b40444
Sjur Fredriksen <sjurtf@ifi.uio.no>
2025-03-27 14:59:37 +0100

    bootstrap netbox in terraform

---
+++ dcim.tf
@@ -0,0 +1,23 @@
+locals {
+  dcim_roles = [
+    { name = "Access switch", color_hex = "ff5722" },
+    { name = "Distro", color_hex = "ff9800" },
+    { name = "Internett ruter", color_hex = "673ab7" },
+    { name = "Leaf", color_hex = "2196f3" },
+    { name = "Spine", color_hex = "aa1409" },
+    { name = "Utskutt distro", color_hex = "9c27b0" },
+  ]
+}
+
+resource "netbox_site" "the_ship" {
+  name    = "Vikingskipet"
+  asn_ids = [for asn in netbox_asn.asns : asn.id]
+}
+
+resource "netbox_device_role" "roles" {
+  for_each  = { for role in local.dcim_roles : replace(lower(role.name), " ", "-") => role }
+  name      = each.value.name
+  color_hex = each.value.color_hex
+  slug      = each.key
+  vm_role   = lookup(each.value, "vm_role", false)
+}
