2a1ed3c292ce3ff2d65a33c74eb59e696b21c4e7
Simen Linderud <simen.linderud@gmail.com>
2025-04-13 23:19:52 +0200

    fixed

--- test-template.py
+++ test-template.py
@@ -1,127 +1,140 @@
-from pickletools import decimalnl_long, decimalnl_short
-
-from jinja2 import Environment, FileSystemLoader, BaseLoader
-from pynetbox import api
-
-import argparse
 import os
-
+import requests
+import uuid
+import argparse
+import re
+from time import sleep
+import json
 NETBOX_TOKEN = os.getenv("NETBOX_API_TOKEN")
 NETBOX_URL = os.getenv("NETBOX_SERVER_URL")
 
-DEVICE_NAME = ""
-nb = ""
-debug = True
+url_config = NETBOX_URL + '/api/extras/config-templates/'
+url_context = NETBOX_URL + '/api/extras/config-contexts/'
 
-class PreprocessedEnvironment(Environment):
-    def get_template(self, name, parent=None, globals=None):
-        template_source = self.loader.get_source(self, name)[0]
-        preprocessed_source = preprocess_template(template_source)
-        return self.from_string(preprocessed_source, globals)
-
-
-def preprocess_template(template_str):
-    replacements = {
-        'device.interfaces.filter': 'device_interfaces_filter',
-        'ipam.Prefix.objects.filter': 'ipam_prefix_objects_filter',
-        'dcim.Device.objects.filter': 'dcim_device_objects_filter',
-        'dcim.Interfaces.objects.filter': 'dcim_interfaces_objects_filter',
-        'interface.ip_addresses.all()': 'interface_ip_addresses_filter(interface_id=interface.id)',
-        '.all()': '',
-        '.type': '.type.value',
-        '.family': '.family.value',
-        '.mode': '.mode.value'
+def nbpost(query):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': "Token " + NETBOX_TOKEN
     }
+    response = requests.post(url_config, headers=headers, json=query)
+    return response.json()
 
-    for search, replace in replacements.items():
-        template_str = template_str.replace(search, replace)
+def nbrender(query, id):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': "Token " + NETBOX_TOKEN
+    }
+    response = requests.post(url_config + f"{str(id)}/render/", headers=headers, json=query)
+    return response.json()
 
-    return template_str
+
+def nbdelete(query):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': "Token " + NETBOX_TOKEN
+    }
+    response = requests.delete(url_config + str(query), headers=headers)
+    return response
+
+
+def nbgetcontexct(query):
+    headers = {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': "Token " + NETBOX_TOKEN
+    }
+    response = requests.get(url_context + str(query), headers=headers)
+    return response
+
+def inline_includes(template_content, template_dir="templates"):
+    """
+    Recursively inlines {% include "somefile" %} statements by replacing them
+    with the file's contents.
+    """
+    pattern = r"{%\s*include\s+['\"]([^'\"]+)['\"]\s*%}"
+    
+    while True:
+        match = re.search(pattern, template_content)
+        if not match:
+            break
+
+        include_file = match.group(1)
+        include_path = os.path.join(template_dir, include_file)
+        
+        if not os.path.exists(include_path):
+            raise FileNotFoundError(f"Included file '{include_file}' not found in {template_dir}.")
+        
+        # Read included file
+        with open(include_path, 'r') as inc:
+            included_text = inc.read()
+        
+        # Recursively process includes in the newly inlined text
+        included_text = inline_includes(included_text, template_dir)
+        
+        # Replace the entire {% include ... %} line with the included text
+        start, end = match.span()
+        template_content = template_content[:start] + included_text + template_content[end:]
+    
+    return template_content
+
 
 def main():
+
     parser = argparse.ArgumentParser(description='Process some integers.')
     parser.add_argument('device_name', type=str, help='Name of the device')
-    parser.add_argument('template_name', type=str, help='Name of the template file found in ../template')
+    parser.add_argument('template_name', type=str, help='Name of the template file found in ../templates')
 
     args = parser.parse_args()
-
-    global nb
-    nb = api(url=NETBOX_URL, token=NETBOX_TOKEN)
-
-    global DEVICE_NAME
-    DEVICE_NAME = args.device_name
+    device_name = args.device_name
     template_file_path = os.path.join('templates', args.template_name)
+
+    context = json.loads(nbgetcontexct("1").content)['data']
 
     if not os.path.exists(template_file_path):
         print(f"The file {template_file_path} does not exist.")
+        return
 
-    loader = FileSystemLoader('.')
-    env = PreprocessedEnvironment(loader=loader, extensions=['jinja2.ext.do'], trim_blocks=True, lstrip_blocks=True)
-    env.globals['device_interfaces_filter'] = device_interfaces_filter
-    env.globals['ipam_prefix_objects_filter'] = ipam_prefixes_filter
-    env.globals['dcim_device_objects_filter'] = dcim_device_objects_filter
-    env.globals['dcim_interfaces_objects_filter'] = dcim_interfaces_objects_filter
-    env.globals['device_primary_ip4'] = device_primary_ip(4)
-    env.globals['device_primary_ip6'] = device_primary_ip(6)
-    env.globals['interface_ip_addresses_filter'] = interface_ip_addresses_all
+    with open(template_file_path, 'r') as f:
+        template = f.read()
 
-    template = env.get_template(template_file_path)
+    # Inline any {% include ... %} statements
+    template = inline_includes(template, template_dir='.')
 
-    device = nb.dcim.devices.get(name=DEVICE_NAME)
-    config = nb.extras.config_contexts.get(id=1).data
-    config["device"] = device
+    # Add the requested lines at the start of the template
+    prelude = "{% set device = dcim.Device.objects.get(name=device) %}"
+    template = prelude + template
 
-    print(template.render(config))
+    # Create temporary config template in NetBox
+    dataz = [{
+        'name': str(uuid.uuid4()),
+        'environment_params': {
+            "extensions": ["jinja2.ext.do"],
+            "trim_blocks": True,
+            "lstrip_blocks": True
+        },
+        'template_code': template
+    }]
+    template_id = nbpost(dataz)[0]['id']
 
-def interface_ip_addresses_all(**kwargs):
-    if debug:
-        print(f"interface_ip_addresses_all: {kwargs}")
-    device = nb.dcim.devices.get(name=DEVICE_NAME)
-    if not device:
-        exit(f"Could not find device with name {DEVICE_NAME}")
+    # Render
+    render_payload = {
+        "device": device_name,
+        "test": True,
+    }
+    render_payload.update(context)
+    result = nbrender(render_payload, template_id)
 
-    addr = list(nb.ipam.ip_addresses.filter(**kwargs))
-    return addr
+    # Print out the rendered content
+    if "content" in result:
+        print(result['content'])
+    else:
+        print(f'No content. Possible error: {result}')
 
-def device_interfaces_filter(**kwargs):
-    if debug:
-        print(f"device_interfaces_filter: {kwargs}")
-    device = nb.dcim.devices.get(name=DEVICE_NAME)
-    if not device:
-        exit(f"Could not find device with name {DEVICE_NAME}")
-    ifs = list(nb.dcim.interfaces.filter(device_id=device.id, **kwargs))
-    return ifs
-
-def ipam_prefixes_filter(**kwargs):
-    if debug:
-        print(f"ipam_prefixes_filter: {kwargs}")
-    prefixes = list(nb.ipam.prefixes.filter(**kwargs))
-    if len(prefixes) < 0:
-        exit(f"Could not find a prefix for filter {kwargs}")
-    if len(prefixes) > 1:
-        exit(f"Found too many prefixes for filter {kwargs}")
-
-    return prefixes[0]
-
-def dcim_device_objects_filter(**kwargs):
-    if debug:
-        print(f"dcim_device_objects_filter: {kwargs}")
-    devices = list(nb.dcim.devices.filter(**kwargs))
-    return devices
-
-def dcim_interfaces_objects_filter(**kwargs):
-    if debug:
-        print(f"dcim_interfaces_objects_filter: {kwargs}")
-    ifs = list(nb.dcim.interfaces.filter(**kwargs))
-    return ifs
-
-def device_primary_ip(version):
-    if version == 4:
-        ip = nb.dcim.devices.get(name=DEVICE_NAME).primary_ip4
-    if version == 6:
-        ip = nb.dcim.devices.get(name=DEVICE_NAME).primary_ip6
-    return ip
-
+    # Delete the temporary template from NetBox
+    nbdelete(template_id)
 
 if __name__ == '__main__':
     main()
