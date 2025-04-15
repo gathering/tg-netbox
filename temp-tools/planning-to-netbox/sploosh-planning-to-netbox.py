883fb0b86bcb57787c47862004e7ce72cde3927a
j-dawg <jonaslindstad@gmail.com>
2025-04-15 13:04:02 +0200

    planning-to-netbox code

--- temp-tools/planning-to-netbox/sploosh-planning-to-netbox.py
+++ temp-tools/planning-to-netbox/sploosh-planning-to-netbox.py
@@ -1,23 +1,39 @@
 from pprint import pprint
+import requests
+import sys, time
 
 dataset = {}
 
 with open('patchlist.txt') as f:
     for line in f.read().splitlines():
-        switch, b, c, d, e = line.split(' ')
+        switch, b, c, d, e = line.split('\t')
         dataset[switch] = {'name': switch}
         dataset[switch]['distro'] = b
         dataset[switch]['uplink1'] = c
         dataset[switch]['uplink2'] = d
         dataset[switch]['uplink3'] = e
 
-with open('switches.txt') as f:
-    for line in f.read().splitlines():
-        switch, v4, v6, v4_mgmt, v6_mgmt, vlan, distro = line.strip().split(' ')
-        dataset[switch]['v4'] = v4
-        dataset[switch]['v6'] = v6
-        dataset[switch]['v4_mgmt'] = v4_mgmt
-        dataset[switch]['v6_mgmt'] = v6_mgmt
-        dataset[switch]['vlan'] = vlan
-
-pprint(dataset)
+n = 0
+headers = {"Authorization": "Token <secret>","Content-Type": "application/json","Accept": "application/json"}
+for _, lol in dataset.items():
+    print(f'\n\nDoing {lol['name']}')
+    n += 1
+    params = {
+        'data': {
+            'switch_name': lol['name'],
+            'device_type': 'EX2200-48T-4G',
+            'device_role': 'Access switch',
+            'uplink_type': '2.5G RJ45',
+            'destination_device_a': lol['distro'],
+            'destination_device_b': '',
+            'destination_interfaces': ','.join([
+                lol['uplink1'],
+                lol['uplink2'],
+                lol['uplink3']
+            ])
+        },
+        'commit': 'true'
+    }
+    resp = requests.post('https://netbox.tg25.tg.no/api/extras/scripts/create-switch.CreateSwitch/', headers = headers, json=params)
+    pprint(resp.json())
+    time.sleep(3)
